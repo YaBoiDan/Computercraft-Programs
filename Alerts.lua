0048cff2aa9ed5901a2babb7f4d027ed94c60fa1
Dan <daniel.hannam@outlook.com>
2026-01-11 20:06:02 +0000

    Correcting output to term and better logging

--- Alerts.lua
+++ Alerts.lua
@@ -1,22 +1,32 @@
 WhisperLogs = false
 RedirectToMon = true
+AlertNoise = false
 
+-- Network Settings
 peripheral.find("modem", rednet.open)
 if not rednet.isOpen() then
     error("No modem found!")
 end
 
+print("LogShower v01")
+
+-- Monitor Setup
+local mon = nil
 if RedirectToMon then
     mon = peripheral.find("monitor")
     if mon then
+        mon.clear()
+        mon.setTextScale(0.5)
+        mon.setCursorPos(1,1)
+        print("Monitor found, redirecting output.")
         term.redirect(mon)
+        print("LogShower v01")
+        print("----------------")
     end
 end
 
-print("LogShower v01")
 
 while true do
     local id, message = rednet.receive(logs_dan)
-    print(("%d: %s"):format(id, message))
-    print("---end---")
+    print(("%d: %s"):format(id, message) .. "[end]")
 end
