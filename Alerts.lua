a84d55a9d5871534d6ec6143a7355f90c339b58b
Dan <daniel.hannam@outlook.com>
2026-01-11 20:30:15 +0000

    Chatbox and tiered alerts

--- Alerts.lua
+++ Alerts.lua
@@ -1,6 +1,7 @@
-WhisperLogs = false
+WhisperLogs = true
 RedirectToMon = true
 AlertNoise = false
+Whisper_Player = "AutomationNerd"
 
 -- Network Settings
 peripheral.find("modem", rednet.open)
@@ -8,7 +9,15 @@ if not rednet.isOpen() then
     error("No modem found!")
 end
 
-print("LogShower v01")
+-- ChatSettings
+if WhisperLogs then
+    chat = peripheral.find("chat_box")
+    if not chat then
+        error("No chat box found!")
+    else
+        print("Chat box found, enabling log whispers.")
+    end
+end
 
 -- Monitor Setup
 local mon = nil
@@ -20,13 +29,29 @@ if RedirectToMon then
         mon.setCursorPos(1,1)
         print("Monitor found, redirecting output.")
         term.redirect(mon)
-        print("LogShower v01")
-        print("----------------")
     end
 end
 
+print("LogShower v01")
+print("----------------")
 
 while true do
     local id, message = rednet.receive(logs_dan)
-    print(("%d: %s"):format(id, message) .. "[end]")
+    -- Extract the type from the message, the value before the first semicolon
+    type = string.match(message, "^(.-);") or "NONE"
+    data = string.gsub(message, "^(.-);", "")
+    print(("%d: [%s] %s"):format(id, type, data) .. "[end]")
+    if chat then
+        if type == "INFO" then
+            toast_colour = "&2&l"
+        elseif type == "WARN" then
+            toast_colour = "&e&l"
+        elseif type == "ERROR" then
+            toast_colour = "&4&l"
+        else
+            toast_colour = "&7&l"
+        end
+        chat.sendToastToPlayer(data, "Log Message", Whisper_Player, (toast_colour .. type))
+    end
+    
 end
