7d5b25bb2ea6988f338bab50f7eb4ec1ce790fd3
Dan <daniel.hannam@outlook.com>
2026-01-11 19:55:23 +0000

    Redirect

--- Alerts.lua
+++ Alerts.lua
@@ -1,6 +1,18 @@
 WhisperLogs = false
+RedirectToMon = true
 
 peripheral.find("modem", rednet.open)
+if not rednet.isOpen() then
+    error("No modem found!")
+end
+
+if RedirectToMon then
+    mon = peripheral.find("monitor")
+    if mon then
+        term.redirect(mon)
+    end
+end
+
 print("LogShower v01")
 
 while true do
